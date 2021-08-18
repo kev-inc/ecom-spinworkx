2184923328f92af884366716af6ee7ae1113330b
Kevin <kevinchanxy@gmail.com>
2021-08-18 21:18:58 +0800

    product

--- pages/product/[itemId].js
+++ pages/product/[itemId].js
@@ -1,53 +1,51 @@
-export default function ProductPage() {
+export default function ProductPage({ data }) {
   return (
     <div className="container">
       <div className="columns">
         <div className="column">
           <figure className="image is-square">
-            <img
-              className="p-4"
-              src="https://www.spinworkx.com/main/wp-content/uploads/2020/04/Bi-Shutter-Galaxy.jpg"
-              alt="thumb"
-            />
+            <img className="p-4" src={data.img[0].src} alt="thumb" />
           </figure>
           <figure class="image is-48x48 is-flex">
-            <img src="https://www.spinworkx.com/main/wp-content/uploads/2020/04/Bi-Shutter-Galaxy.jpg" />
-            <img src="https://www.spinworkx.com/main/wp-content/uploads/2020/04/Bi-Shutter-Silver.jpg" />
-            <img src="https://www.spinworkx.com/main/wp-content/uploads/2020/04/Bi-Shutter-Gap.jpg" />
+            {data.img.map((im) => (
+              <img src={im.thumbnail} alt="preview" />
+            ))}
           </figure>
         </div>
         <div className="column">
-          <h2 className="is-size-2 has-text-weight-medium my-2">
-            Bi-Metal Shutter
+          <h2 className="is-size-2 has-text-weight-medium block">
+            {data.name}
           </h2>
-          <h4 className="is-size-4 my-2">US$49.99</h4>
-          <p className="my-4">
-            Skill Level: Advance (Unresponsive)
-            <br />
-            Company / Brand: YoYoFactory
-            <br />
-            Diameter: 56.0mm
-            <br />
-            Width: 46.5mm
-            <br />
-            Weight: 65.0grams
-            <br />
-            Bearing Size: Size C (Large)
-            <br />
-            Response: Silicone Pads (Large Bearing)
-            <br />
-          </p>
-          <div className="my-4">
-            <span>Categories: </span>
-            <a href="#">New Arrivals</a>
-            <span>, </span>
-            <a href="#">Yo Yo</a>
-            <span>, </span>
-            <a href="#">YoYoFactory</a>
+          <h4 className="is-size-4 block">{data.price}</h4>
+          <div className="block">
+            <p className="mb-3" style={{ whiteSpace: "pre-line" }}>
+              {data.desc}
+            </p>
+            <div>
+              <span>Categories: </span>
+              {data.tags.map((tag) => (
+                <a href="#">{tag}; </a>
+              ))}
+            </div>
           </div>
-          <button class="button is-black is-fullwidth my-2">Add to Cart</button>
+          <div className="block">
+            <button class="button is-black is-fullwidth my-2">
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { itemId } = context.params;
+  const res = await fetch(
+    `https://scrapi-go.vercel.app/api/spinworkx/product?name=${itemId}`
+  );
+  const data = await res.json();
+
+  // Pass data to the page via props
+  return { props: { data } };
 }
